15d30986cb0c5d661021d1141de7193c33d4cacf
OniOniOn- <0onionion0@gmail.com>
2023-02-10 15:39:34 +0900

    feat: remove unused gearproptype

---
+++ packages/gear/src/tests/gearproptype.spec.ts
@@ -0,0 +1,24 @@
+import { GearPropType } from "../lib/gearproptype";
+
+describe("GearPropType value tests", () => {
+  // changing these value is a breaking change
+  // since serializing/deserializing GearPropType values might break
+  it.each([
+    [GearPropType.incSTR, 1],
+    [GearPropType.incAUT, 16],
+    [GearPropType.damR, 28],
+    [GearPropType.incCHUC, 31],
+    [GearPropType.incPADr, 100],
+    [GearPropType.incMADr, 101],
+    [GearPropType.incTerR, 124],
+    [GearPropType.incCriticaldamage, 135],
+    [GearPropType.attackSpeed, 200],
+    [GearPropType.reduceReq, 203],
+    [GearPropType.only, 1100],
+    [GearPropType.craftEXP, 1111],
+    [GearPropType.blockGoldHammer, 1122],
+    [GearPropType.tucIgnoreForPotential, 1126],
+  ])("'%d' == '%d'", (type, expected) => {
+    expect(type).toBe(expected);
+  });
+});
