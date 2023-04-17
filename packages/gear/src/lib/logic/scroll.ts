af21c5b0fd87947bdd8ea4b035e9ddca373e0ffc
OniOniOn- <0onionion0@gmail.com>
2023-04-17 17:47:34 +0900

    feat: add armor 15 spelltrace

--- packages/gear/src/lib/logic/scroll.ts
+++ packages/gear/src/lib/logic/scroll.ts
@@ -25,7 +25,7 @@ export interface Scroll {
   /** 주문서 이름 */
   name: string;
   /** 주문서 옵션 */
-  stat: Map<GearPropType, number>;
+  option: Map<GearPropType, number>;
 }
 
 /**
@@ -41,253 +41,292 @@ export function getSpellTraceScroll(
   type: SpellTraceStatType,
   probability: SpellTraceProbability
 ): Scroll | undefined {
-  const attackType =
-    type === GearPropType.incINT ? GearPropType.incMAD : GearPropType.incPAD;
-  const tier = gear.req.level >= 115 ? 2 : gear.req.level >= 75 ? 1 : 0;
-
-  let statName: string;
-  const attackName = attackType === GearPropType.incMAD ? "마력" : "공격력";
-  switch (type) {
-    case GearPropType.incSTR:
-      statName = "힘";
-      break;
-    case GearPropType.incDEX:
-      statName = "민첩";
-      break;
-    case GearPropType.incINT:
-      statName = "지력";
-      break;
-    case GearPropType.incLUK:
-      statName = "운";
-      break;
-    case GearPropType.incAllStat:
-      statName = "올스탯";
-      break;
-    case GearPropType.incMHP:
-      statName = "체력";
-      break;
-  }
-
   // weapon like
   if (Gear.isWeapon(gear.type) || gear.type === GearType.katara) {
-    if (type === GearPropType.incAllStat) {
-      return undefined;
-    }
-    let data: number[][];
-    switch (probability) {
-      case 100:
-        data = [
-          [1, 0],
-          [2, 0],
-          [3, 1],
-        ];
-        break;
-      case 70:
-        data = [
-          [2, 0],
-          [3, 1],
-          [5, 2],
-        ];
-        break;
-      case 30:
-        data = [
-          [3, 1],
-          [5, 2],
-          [7, 3],
-        ];
-        break;
-      case 15:
-        data = [
-          [5, 2],
-          [7, 3],
-          [9, 4],
-        ];
-        break;
-    }
-    const attackValue = data[tier][0];
-    const statValue = data[tier][1] * (type === GearPropType.incMHP ? 50 : 1);
-    const name =
-      statValue > 0
-        ? `${probability}% ${attackName}(${statName}) 주문서`
-        : `${probability}% ${attackName} 주문서`;
-    const stat = new Map([
-      [type, statValue],
-      [attackType, attackValue],
-    ]);
-
-    return { name, stat };
-  }
-  // prob 15% for non weapon like type
-  else if (probability === 15) {
-    return undefined;
+    return getWeaponSpellTrace(gear, type, probability);
   }
   // gloves
   if (gear.type === GearType.glove) {
-    let data: number[];
-    switch (probability) {
-      case 100:
-        data = [0, 1, 1];
-        break;
-      case 70:
-        data = [1, 2, 2];
-        break;
-      case 30:
-        data = [2, 3, 3];
-        break;
-    }
-    const value = data[tier];
-    let name: string;
-    let stat: Map<GearPropType, number>;
-    if (value === 0) {
-      name = `${probability}% 방어력 주문서`;
-      stat = new Map([[GearPropType.incPDD, 3]]);
-    } else {
-      name = `${probability}% ${attackName} 주문서`;
-      stat = new Map([[attackType, value]]);
-    }
-    return { name, stat };
+    return getGloveSpellTrace(gear, type, probability);
   }
   // armor
   if (Gear.isArmor(gear.type) || gear.type === GearType.shoulder) {
-    // armor all stat
-    if (type === GearPropType.incAllStat) {
-      if (probability !== 30) {
-        return undefined;
-      }
-      const data = [
-        [1, 30, 4],
-        [2, 70, 7],
-        [3, 120, 10],
-      ];
-      const statValue = data[tier][0];
-      const mhpValue = data[tier][1];
-      const pddValue = data[tier][2];
-
-      const name = `${probability}% ${statName} 주문서`;
-      const stat = new Map([
-        [GearPropType.incSTR, statValue],
-        [GearPropType.incDEX, statValue],
-        [GearPropType.incINT, statValue],
-        [GearPropType.incLUK, statValue],
-        [GearPropType.incMHP, mhpValue],
-        [GearPropType.incPDD, pddValue],
-      ]);
-      if (gear.upgradeCount === 3) {
-        if (gear.req.job === 0) {
-          stat.set(GearPropType.incMAD, 1);
-          stat.set(GearPropType.incPAD, 1);
-        } else if (Math.floor(gear.req.job / 2) % 2 === 1)
-          stat.set(GearPropType.incMAD, 1);
-        else stat.set(GearPropType.incPAD, 1);
-      }
-      return { name, stat };
-    }
-    // armor single stat
-    let data: number[][];
-    switch (probability) {
-      case 100:
-        data = [
-          [1, 5, 1],
-          [2, 20, 2],
-          [3, 30, 3],
-        ];
-        break;
-      case 70:
-        data = [
-          [2, 15, 2],
-          [3, 40, 4],
-          [4, 70, 5],
-        ];
-        break;
-      case 30:
-        data = [
-          [3, 30, 4],
-          [5, 70, 7],
-          [7, 120, 10],
-        ];
-        break;
-    }
-    const statValue = type !== GearPropType.incMHP ? data[tier][0] : 0;
-    const mhpValue =
-      data[tier][1] + (type === GearPropType.incMHP ? data[tier][0] * 50 : 0);
-    const pddValue = data[tier][2];
-
-    const name = `${probability}% ${statName} 주문서`;
-    const stat = new Map([
-      [GearPropType.incMHP, mhpValue],
-      [GearPropType.incPDD, pddValue],
-    ]);
-    if (statValue > 0) {
-      stat.set(type, statValue);
-    }
-    if (gear.upgradeCount === 3) {
-      if (gear.req.job === 0) {
-        stat.set(GearPropType.incMAD, 1);
-        stat.set(GearPropType.incPAD, 1);
-      } else if (Math.floor(gear.req.job / 2) % 2 === 1)
-        stat.set(GearPropType.incMAD, 1);
-      else stat.set(GearPropType.incPAD, 1);
-    }
-    return { name, stat };
+    return getArmorSpellTrace(gear, type, probability);
   }
   // acc
   if (Gear.isAccessory(gear.type)) {
-    if (type === GearPropType.incAllStat) {
-      if (probability !== 30) {
-        return undefined;
-      }
-      const data = [
-        [1, 30, 4],
-        [2, 70, 7],
-        [3, 120, 10],
-      ];
-      const statValue = data[tier][0];
-
-      const name = `${probability}% ${statName} 주문서`;
-      const stat = new Map([
-        [GearPropType.incSTR, statValue],
-        [GearPropType.incDEX, statValue],
-        [GearPropType.incINT, statValue],
-        [GearPropType.incLUK, statValue],
-      ]);
-      return { name, stat };
-    }
-    let data: number[];
-    switch (probability) {
-      case 100:
-        data = [1, 1, 2];
-        break;
-      case 70:
-        data = [2, 2, 3];
-        break;
-      case 30:
-        data = [3, 4, 5];
-        break;
-    }
-    const value = data[tier] * (type === GearPropType.incMHP ? 50 : 1);
-    const name = `${probability}% ${statName} 주문서`;
-    const stat = new Map([[type, value]]);
-    return { name, stat };
+    return getAccSpellTrace(gear, type, probability);
   }
   // heart
   if (gear.type === GearType.machineHeart) {
-    let data: number[];
-    switch (probability) {
-      case 100:
-        data = [1, 2, 3];
-        break;
-      case 70:
-        data = [2, 3, 5];
-        break;
-      case 30:
-        data = [3, 5, 7];
-        break;
-    }
-    const value = data[tier];
-    const name = `${probability}% ${statName} 주문서`;
-    const stat = new Map([[attackType, value]]);
-    return { name, stat };
+    return getHeartSpellTrace(gear, type, probability);
   }
 
   return undefined;
+}
+
+const weaponAttStatData = {
+  100: [
+    [1, 0],
+    [2, 0],
+    [3, 1],
+  ],
+  70: [
+    [2, 0],
+    [3, 1],
+    [5, 2],
+  ],
+  30: [
+    [3, 1],
+    [5, 2],
+    [7, 3],
+  ],
+  15: [
+    [5, 2],
+    [7, 3],
+    [9, 4],
+  ],
+};
+
+const armorStatMhpPddData = {
+  100: [
+    [1, 5, 1],
+    [2, 20, 2],
+    [3, 30, 3],
+  ],
+  70: [
+    [2, 15, 2],
+    [3, 40, 4],
+    [4, 70, 5],
+  ],
+  30: [
+    [3, 30, 4],
+    [5, 70, 7],
+    [7, 120, 10],
+  ],
+  15: [
+    [4, 45, 6],
+    [7, 110, 10],
+    [10, 170, 15],
+  ],
+};
+
+const armorAllStatData = {
+  30: [1, 2, 3],
+  15: [2, 3, 4],
+};
+
+const accAllStatData = {
+  30: [1, 2, 3],
+};
+
+const gloveAttData = {
+  100: [0, 1, 1],
+  70: [1, 2, 2],
+  30: [2, 3, 3],
+  15: [3, 4, 4],
+};
+
+const accStatData = {
+  100: [1, 1, 2],
+  70: [2, 2, 3],
+  30: [3, 4, 5],
+};
+
+const heartAttData = {
+  100: [1, 2, 3],
+  70: [2, 3, 5],
+  30: [3, 5, 7],
+};
+
+const attTypes = {
+  [GearPropType.incSTR]: GearPropType.incPAD,
+  [GearPropType.incDEX]: GearPropType.incPAD,
+  [GearPropType.incINT]: GearPropType.incMAD,
+  [GearPropType.incLUK]: GearPropType.incPAD,
+  [GearPropType.incMHP]: GearPropType.incPAD,
+  [GearPropType.incAllStat]: GearPropType.incPAD,
+} as const;
+
+const typeNames = {
+  [GearPropType.incSTR]: "힘",
+  [GearPropType.incDEX]: "민첩",
+  [GearPropType.incINT]: "지력",
+  [GearPropType.incLUK]: "운",
+  [GearPropType.incMHP]: "체력",
+  [GearPropType.incAllStat]: "올스탯",
+  [GearPropType.incPAD]: "공격력",
+  [GearPropType.incMAD]: "마력",
+} as const;
+
+function getSpellTraceTier(gear: Gear): 0 | 1 | 2 {
+  if (gear.req.level > 110) return 2;
+  else if (gear.req.level > 70) return 1;
+  else return 0;
+}
+
+function getWeaponSpellTrace(
+  gear: Gear,
+  type: SpellTraceStatType,
+  probability: SpellTraceProbability
+): Scroll | undefined {
+  if (type === GearPropType.incAllStat) {
+    return undefined;
+  }
+
+  const tier = getSpellTraceTier(gear);
+  // eslint-disable-next-line prefer-const
+  let [att, stat] = weaponAttStatData[probability][tier];
+
+  const attType = attTypes[type];
+  const attName = typeNames[attType];
+  if (type === GearPropType.incMHP) stat *= 50;
+
+  const name =
+    stat > 0
+      ? `${probability}% ${attName}(${typeNames[type]}) 주문서`
+      : `${probability}% ${attName} 주문서`;
+  const option = new Map([
+    [type, stat],
+    [attType, att],
+  ]);
+
+  return { name, option };
+}
+
+function getArmorSpellTrace(
+  gear: Gear,
+  type: SpellTraceStatType,
+  probability: SpellTraceProbability
+): Scroll | undefined {
+  const tier = getSpellTraceTier(gear);
+  const [stat, mhp, pdd] = armorStatMhpPddData[probability][tier];
+
+  let statArr: [GearPropType, number][] = [];
+  let mhpArr: [GearPropType, number][] = [];
+  let attArr: [GearPropType, number][] = [];
+
+  if (type === GearPropType.incMHP) {
+    // MHP
+    mhpArr = [[GearPropType.incMHP, mhp + stat * 50]];
+  } else {
+    mhpArr = [[GearPropType.incMHP, mhp]];
+    if (type === GearPropType.incAllStat) {
+      // AllStat
+      if (!(probability in armorAllStatData)) return undefined;
+      // @ts-expect-error probability is checked already
+      const stat = armorAllStatData[probability][tier];
+      statArr = [
+        [GearPropType.incSTR, stat],
+        [GearPropType.incDEX, stat],
+        [GearPropType.incINT, stat],
+        [GearPropType.incLUK, stat],
+      ];
+    } else {
+      // STR, DEX, INT, LUK
+      statArr = [[type, stat]];
+    }
+  }
+
+  if (gear.upgradeCount === 3) {
+    if (gear.req.job === 0) {
+      attArr = [
+        [GearPropType.incPAD, 1],
+        [GearPropType.incMAD, 1],
+      ];
+    } else if (Math.floor(gear.req.job / 2) % 2 === 1) {
+      attArr = [[GearPropType.incMAD, 1]];
+    } else {
+      attArr = [[GearPropType.incPAD, 1]];
+    }
+  }
+
+  const name = `${probability}% ${typeNames[type]} 주문서`;
+  const option = new Map([
+    ...statArr,
+    ...attArr,
+    ...mhpArr,
+    [GearPropType.incPDD, pdd],
+  ]);
+
+  return { name, option };
+}
+
+function getGloveSpellTrace(
+  gear: Gear,
+  type: SpellTraceStatType,
+  probability: SpellTraceProbability
+): Scroll | undefined {
+  const tier = getSpellTraceTier(gear);
+  const att = gloveAttData[probability][tier];
+
+  if (att === 0) {
+    return {
+      name: `${probability}% 방어력 주문서`,
+      option: new Map([[GearPropType.incPDD, 3]]),
+    };
+  } else {
+    const attType = attTypes[type];
+    return {
+      name: `${probability}% ${typeNames[attType]} 주문서`,
+      option: new Map([[attType, att]]),
+    };
+  }
+}
+
+function getAccSpellTrace(
+  gear: Gear,
+  type: SpellTraceStatType,
+  probability: SpellTraceProbability
+): Scroll | undefined {
+  if (!(probability in accStatData)) return undefined;
+
+  const tier = getSpellTraceTier(gear);
+  // @ts-expect-error probability is checked already
+  const stat = accStatData[probability][tier];
+
+  let statArr: [GearPropType, number][] = [];
+
+  if (type === GearPropType.incMHP) {
+    statArr = [[GearPropType.incMHP, stat * 50]];
+  } else {
+    if (type === GearPropType.incAllStat) {
+      // AllStat
+      if (!(probability in accAllStatData)) return undefined;
+      // @ts-expect-error probability is checked already
+      const stat = accAllStatData[probability][tier];
+      statArr = [
+        [GearPropType.incSTR, stat],
+        [GearPropType.incDEX, stat],
+        [GearPropType.incINT, stat],
+        [GearPropType.incLUK, stat],
+      ];
+    } else {
+      // STR, DEX, INT, LUK
+      statArr = [[type, stat]];
+    }
+  }
+
+  const name = `${probability}% ${typeNames[type]} 주문서`;
+  const option = new Map(statArr);
+
+  return { name, option };
+}
+
+function getHeartSpellTrace(
+  gear: Gear,
+  type: SpellTraceStatType,
+  probability: SpellTraceProbability
+): Scroll | undefined {
+  if (!(probability in heartAttData)) return undefined;
+
+  const tier = getSpellTraceTier(gear);
+  // @ts-expect-error probability is checked already
+  const att = heartAttData[probability][tier];
+  const attType = attTypes[type];
+
+  const name = `${probability}% ${typeNames[type]} 주문서`;
+  const option = new Map([[attType, att]]);
+
+  return { name, option };
 }
--- packages/gear/src/tests/logic/scroll.spec.ts
+++ packages/gear/src/tests/logic/scroll.spec.ts
@@ -17,30 +17,30 @@ describe("spell trace normal", () => {
 
     let scroll: Scroll | undefined;
     scroll = getSpellTraceScroll(gear, GearPropType.incSTR, 100);
-    expect(scroll?.stat.get(GearPropType.incSTR)).toBe(1);
-    expect(scroll?.stat.get(GearPropType.incMHP)).toBe(5);
-    expect(scroll?.stat.get(GearPropType.incPDD)).toBe(1);
+    expect(scroll?.option.get(GearPropType.incSTR)).toBe(1);
+    expect(scroll?.option.get(GearPropType.incMHP)).toBe(5);
+    expect(scroll?.option.get(GearPropType.incPDD)).toBe(1);
     expect(scroll?.name).toBe("100% 힘 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incLUK, 70);
-    expect(scroll?.stat.get(GearPropType.incLUK)).toBe(2);
+    expect(scroll?.option.get(GearPropType.incLUK)).toBe(2);
     expect(scroll?.name).toBe("70% 운 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incINT, 30);
-    expect(scroll?.stat.get(GearPropType.incINT)).toBe(3);
-    expect(scroll?.stat.get(GearPropType.incMHP)).toBe(30);
-    expect(scroll?.stat.get(GearPropType.incPDD)).toBe(4);
+    expect(scroll?.option.get(GearPropType.incINT)).toBe(3);
+    expect(scroll?.option.get(GearPropType.incMHP)).toBe(30);
+    expect(scroll?.option.get(GearPropType.incPDD)).toBe(4);
     expect(scroll?.name).toBe("30% 지력 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incMHP, 30);
-    expect(scroll?.stat.get(GearPropType.incMHP)).toBe(180);
+    expect(scroll?.option.get(GearPropType.incMHP)).toBe(180);
     expect(scroll?.name).toBe("30% 체력 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incAllStat, 30);
-    expect(scroll?.stat.get(GearPropType.incSTR)).toBe(1);
-    expect(scroll?.stat.get(GearPropType.incDEX)).toBe(1);
-    expect(scroll?.stat.get(GearPropType.incMHP)).toBe(30);
-    expect(scroll?.stat.get(GearPropType.incPDD)).toBe(4);
+    expect(scroll?.option.get(GearPropType.incSTR)).toBe(1);
+    expect(scroll?.option.get(GearPropType.incDEX)).toBe(1);
+    expect(scroll?.option.get(GearPropType.incMHP)).toBe(30);
+    expect(scroll?.option.get(GearPropType.incPDD)).toBe(4);
     expect(scroll?.name).toBe("30% 올스탯 주문서");
   });
   it("medium req level weapon scroll name / stat", () => {
@@ -56,23 +56,23 @@ describe("spell trace normal", () => {
 
     let scroll: Scroll | undefined;
     scroll = getSpellTraceScroll(gear, GearPropType.incINT, 100);
-    expect(scroll?.stat.get(GearPropType.incINT)).toBe(0);
-    expect(scroll?.stat.get(GearPropType.incMAD)).toBe(2);
+    expect(scroll?.option.get(GearPropType.incINT)).toBe(0);
+    expect(scroll?.option.get(GearPropType.incMAD)).toBe(2);
     expect(scroll?.name).toBe("100% 마력 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incINT, 70);
-    expect(scroll?.stat.get(GearPropType.incINT)).toBe(1);
-    expect(scroll?.stat.get(GearPropType.incMAD)).toBe(3);
+    expect(scroll?.option.get(GearPropType.incINT)).toBe(1);
+    expect(scroll?.option.get(GearPropType.incMAD)).toBe(3);
     expect(scroll?.name).toBe("70% 마력(지력) 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incINT, 30);
-    expect(scroll?.stat.get(GearPropType.incINT)).toBe(2);
-    expect(scroll?.stat.get(GearPropType.incMAD)).toBe(5);
+    expect(scroll?.option.get(GearPropType.incINT)).toBe(2);
+    expect(scroll?.option.get(GearPropType.incMAD)).toBe(5);
     expect(scroll?.name).toBe("30% 마력(지력) 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incINT, 15);
-    expect(scroll?.stat.get(GearPropType.incINT)).toBe(3);
-    expect(scroll?.stat.get(GearPropType.incMAD)).toBe(7);
+    expect(scroll?.option.get(GearPropType.incINT)).toBe(3);
+    expect(scroll?.option.get(GearPropType.incMAD)).toBe(7);
     expect(scroll?.name).toBe("15% 마력(지력) 주문서");
   });
   it("high req level accessory scroll name / stat", () => {
@@ -92,24 +92,24 @@ describe("spell trace normal", () => {
 
     let scroll: Scroll | undefined;
     scroll = getSpellTraceScroll(gear, GearPropType.incSTR, 100);
-    expect(scroll?.stat.get(GearPropType.incSTR)).toBe(2);
+    expect(scroll?.option.get(GearPropType.incSTR)).toBe(2);
     expect(scroll?.name).toBe("100% 힘 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incDEX, 70);
-    expect(scroll?.stat.get(GearPropType.incDEX)).toBe(3);
+    expect(scroll?.option.get(GearPropType.incDEX)).toBe(3);
     expect(scroll?.name).toBe("70% 민첩 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incINT, 30);
-    expect(scroll?.stat.get(GearPropType.incINT)).toBe(5);
-    expect(scroll?.stat.get(GearPropType.incPAD)).toBeUndefined();
+    expect(scroll?.option.get(GearPropType.incINT)).toBe(5);
+    expect(scroll?.option.get(GearPropType.incPAD)).toBeUndefined();
     expect(scroll?.name).toBe("30% 지력 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incMHP, 100);
-    expect(scroll?.stat.get(GearPropType.incMHP)).toBe(100);
+    expect(scroll?.option.get(GearPropType.incMHP)).toBe(100);
     expect(scroll?.name).toBe("100% 체력 주문서");
 
     scroll = getSpellTraceScroll(gear, GearPropType.incAllStat, 30);
-    expect(scroll?.stat.get(GearPropType.incSTR)).toBe(3);
+    expect(scroll?.option.get(GearPropType.incSTR)).toBe(3);
     expect(scroll?.name).toBe("30% 올스탯 주문서");
   });
 });
