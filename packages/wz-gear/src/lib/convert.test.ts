3d79bebfa9b16f1bc0d0346c702efc09c1ebbdab
Lechros <0onionion0@gmail.com>
2025-04-19 22:12:13 +0900

    test: comment out convert function test for future reference

--- packages/wz-gear/src/lib/convert.test.ts
+++ packages/wz-gear/src/lib/convert.test.ts
@@ -1,28 +1,28 @@
-import { convert } from './convert';
-import _expectedData from './res/gear-data-expected.json';
-import _rawData from './res/gear-data-raw.json';
-import { WzGear } from './wz';
+// import { convert } from './convert';
+// import _expectedData from './res/gear-data-expected.json';
+// import _rawData from './res/gear-data-raw.json';
+// import { WzGear } from './wz';
 
-const rawData = _rawData as Record<string, WzGear>;
-const expectedData = _expectedData as Record<string, unknown>;
+// const rawData = _rawData as Record<string, WzGear>;
+// const expectedData = _expectedData as Record<string, unknown>;
 
-test.skip('Convert result is equal to WzJson version', () => {
-  for (const [id, data] of Object.entries<WzGear>(rawData)) {
-    const expected = expectedData[id];
-    if (typeof expected === 'object' && expected !== null) {
-      if ('potentials' in expected) {
-        (expected as { potentials: unknown[] }).potentials = (
-          expected as { potentials: unknown[] }
-        ).potentials.filter((pot) => pot);
-      }
-      if ('attributes' in expected && typeof expected.attributes === 'object') {
-        delete (expected as { attributes: { blockGoldenHammer?: boolean } })
-          .attributes.blockGoldenHammer;
-      }
-    }
+// test.skip('Convert result is equal to WzJson version', () => {
+//   for (const [id, data] of Object.entries<WzGear>(rawData)) {
+//     const expected = expectedData[id];
+//     if (typeof expected === 'object' && expected !== null) {
+//       if ('potentials' in expected) {
+//         (expected as { potentials: unknown[] }).potentials = (
+//           expected as { potentials: unknown[] }
+//         ).potentials.filter((pot) => pot);
+//       }
+//       if ('attributes' in expected && typeof expected.attributes === 'object') {
+//         delete (expected as { attributes: { blockGoldenHammer?: boolean } })
+//           .attributes.blockGoldenHammer;
+//       }
+//     }
 
-    const actual = convert(data);
+//     const actual = convert(data);
 
-    expect(actual).toEqual(expected);
-  }
-});
+//     expect(actual).toEqual(expected);
+//   }
+// });
