e501ede2005ab0d17250bf4030a12881db6b73ac
Lechros <0onionion0@gmail.com>
2025-04-20 17:14:52 +0900

    test(WIP): create test for gear can generation

--- packages/wz-gear/src/lib/convert.test.ts
+++ packages/wz-gear/src/lib/convert.test.ts
@@ -26,3 +26,81 @@
 //     expect(actual).toEqual(expected);
 //   }
 // });
+
+import { AddOptionCan, Gear, PotentialCan } from '@malib/gear';
+import { convert } from './convert';
+import _gearData from './res/gear-data.json';
+import { WzGear } from './wz';
+
+const gearData = _gearData as Record<string, WzGear>;
+
+const CANNOT = 0;
+const CAN = 1;
+const FIXED = 2;
+
+type Can = 0 | 1 | 2;
+
+interface Cans {
+  cannotUpgrade: boolean;
+  canAddOption: Can;
+  canPotential: Can;
+  canAdditionalPotential: Can;
+  maxStar: number;
+}
+
+test('TEMP: Generate gearData can', () => {
+  const result: Record<string, Cans> = {};
+  for (const [id, info] of Object.entries(gearData)) {
+    const data = convert(info);
+    const gear = new Gear(data);
+    const can: Cans = {
+      cannotUpgrade: gear.attributes.cannotUpgrade,
+      canAddOption: chain(
+        () => toCan(gear.attributes.canAddOption),
+        gear.supportsAddOption ? CAN : CANNOT,
+      ),
+      canPotential: chain(
+        () => toCan(gear.attributes.canPotential),
+        gear.supportsPotential ? CAN : CANNOT,
+      ),
+      canAdditionalPotential: chain(
+        () => toCan(gear.attributes.canAdditionalPotential),
+        gear.supportsAdditionalPotential ? CAN : CANNOT,
+      ),
+      maxStar: gear.maxStar,
+    };
+    result[id] = can;
+  }
+  // RUN WITH> yarn vitest run .\packages\wz-gear\src\lib\convert.test.ts > result.json
+  console.log(JSON.stringify(result, null, 2));
+});
+
+function chain<T>(...args: (T | (() => T | undefined))[]): T {
+  for (const arg of args) {
+    if (typeof arg === 'function') {
+      const result = (arg as () => T | undefined)();
+      if (result !== undefined) {
+        return result;
+      }
+    } else {
+      return arg;
+    }
+  }
+  throw new Error('No value found');
+}
+
+function toCan(value: AddOptionCan | PotentialCan): Can | undefined {
+  switch (value) {
+    case AddOptionCan.Can:
+    case PotentialCan.Can:
+      return CAN;
+    case AddOptionCan.Cannot:
+    case PotentialCan.Cannot:
+      return CANNOT;
+    case AddOptionCan.Fixed:
+    case PotentialCan.Fixed:
+      return FIXED;
+    default:
+      return undefined;
+  }
+}
